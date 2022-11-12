a18b2765ce41fcbce8132c82fb9a05caf99473d2
Pablofu9 <pablofuertesruiz@gmail.com>
2022-11-12 11:54:42 +0100

    Agregar archivos de proyecto.

---
+++ MarcadorFinal/Conexion/Conexion.cs
@@ -0,0 +1,40 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MarcadorFinal.Conexion
+{
+    public class Conexion
+    {
+        private MySqlConnection con = new MySqlConnection();
+
+        static string server = "localhost";
+        static string database = "marcador";
+        static string user = "root";
+        static string password = "pabloygala96";
+        static string puerto = "3306";
+
+        string cadenaConexion = "server=" + server + ";" + "port=" + puerto + ";" + "user_id=" + user + ";" +
+            " password=" + password +";"+"database="+database+";";
+
+        public MySqlConnection establecerConexion()
+        {
+            try
+            {
+                con.ConnectionString = cadenaConexion;
+                con.Open();
+                MessageBox.Show("Se pudo conextar a la base");
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("No se pudo conectar "+e.ToString());
+            }
+            return con;
+        }
+    }
+    
+}
---
+++ MarcadorFinal/Vista/Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MarcadorFinal.Vista
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(Object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+            
+        }
+        private void btnMinimizar_Click(Object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnCerrar_Click(Object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAcceder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
